490cd8085b7de7faa459a1c9f6cfff6253b8b596
andrehebra <andrehebra@gmail.com>
2023-07-07 17:44:17 -0400

    working now

--- an083590.c
+++ an083590.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int bubbleSort(int *array, int length) {
+int* bubbleSort(int *array, int length) {
+
+    int *track = malloc(sizeof(int) * 500);
+
+    for (int i = 0; i < length; i++) {
+        track[i] = 0;
+    }
 
     int temp;
     int sorted = 0;
-    int count = 0;
+    int count = 1;
 
     while (sorted != length - 1) {
 
@@ -17,14 +23,17 @@ int bubbleSort(int *array, int length) {
                 array[i] = array[i + 1];
                 array[i + 1] = temp;
 
-                count++;
+                track[count]++;
             } else {
                 sorted++;
             }
         }
+        count++;
     }
 
-    return count;
+    track[0] = count-1;
+
+    return track;
     
 }
 
@@ -40,17 +49,18 @@ int main() {
 
     int array[9] = {97,  16,  45,  63,  13,  22,  7 , 58 , 72};
     int length = 9;
-    int count = 0;
 
     printf("Original Array:  ");
     printArray(array, length);
 
-    count = bubbleSort(array, length);
+    int *count = bubbleSort(array, length);
 
     printf("Sorted Array:    ");
     printArray(array, length);
 
-    printf("\nNumber of Swaps: %3d\n", count);
+    printf("Number of Swaps: ");
+
+    printArray(&count[1], count[0]);
 
 
 
